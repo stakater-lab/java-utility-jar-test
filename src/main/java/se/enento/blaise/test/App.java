adbb0f911102df3668fa2e4ed63ad21b7ad1fba5
Jan Hallonstén <github@hallonsten.nu>
2020-11-18 17:10:46 +0100

    Initial version

---
+++ src/main/java/se/enento/blaise/test/App.java
@@ -0,0 +1,13 @@
+package se.enento.blaise.test;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
